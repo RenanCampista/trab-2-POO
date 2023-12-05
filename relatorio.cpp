3d2818e1772ec7fdbbafb7255009bdcfa4e9e477
Renan Campista <renan.campistah@hotmail.com>
2023-12-05 19:02:37 -0300

    atualizando saida de alguns relatorios

--- relatorio.cpp
+++ relatorio.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "relatorio.h"
 
 using namespace std;
@@ -62,6 +63,31 @@ void Relatorio::eleitos_por_faixa_etaria() {
     int idade, eleitos = this->numero_total_eleitos;
     int cont_menor_30 = 0, cont_30_40 = 0, cont_40_50 = 0, cont_50_60 = 0, cont_maior_60 = 0;
 
+    for (const Candidato &c : this->candidatos_vector) {
+        idade = c.get_idade(this->data_eleicao);
+        if (c.is_eleito()) {
+            if (idade < 30) cont_menor_30++;
+            else if (idade < 40) cont_30_40++;
+            else if (idade < 50) cont_40_50++;
+            else if (idade < 60) cont_50_60++;
+            else cont_maior_60++;
+        }
+    }
+
+    // System.out.println("      Idade < 30: " + contMenor30 + " (" + (eleitos == 0 ? "0" : df.format((double) contMenor30 / eleitos * 100)) + "%)");
+    // System.out.println("30 <= Idade < 40: " + cont30a39 + " (" + (eleitos == 0 ? "0" : df.format((double) cont30a39 / eleitos * 100)) + "%)");
+    // System.out.println("40 <= Idade < 50: " + cont40a49 + " (" + (eleitos == 0 ? "0" : df.format((double) cont40a49 / eleitos * 100)) + "%)");
+    // System.out.println("50 <= Idade < 60: " + cont50a59 + " (" + (eleitos == 0 ? "0" : df.format((double) cont50a59 / eleitos * 100)) + "%)");
+    // System.out.println("60 <= Idade\t: " + cont60 + " (" + (eleitos == 0 ? "0" : df.format((double) cont60 / eleitos * 100)) + "%)");
+
+    cout << fixed << setprecision(2);
+
+    cout << "      Idade < 30: " << cont_menor_30 << " (" << (eleitos == 0 ? 0 : (double) cont_menor_30 / eleitos * 100) << "%)\n";
+    cout << "30 <= Idade < 40: " << cont_30_40 << " (" << (eleitos == 0 ? 0 : (double) cont_30_40 / eleitos * 100) << "%)\n";
+    cout << "40 <= Idade < 50: " << cont_40_50 << " (" << (eleitos == 0 ? 0 : (double) cont_40_50 / eleitos * 100) << "%)\n";
+    cout << "50 <= Idade < 60: " <<  cont_50_60 << " (" << (eleitos == 0 ? 0 : (double)  cont_50_60 / eleitos * 100) << "%)\n";
+    cout << "60 <= Idade\t: " << cont_maior_60 << " (" << (eleitos == 0 ? 0 : (double) cont_maior_60 / eleitos * 100) << "%)\n";
+
 }
 
 //Relatorio 9
@@ -72,13 +98,19 @@ void Relatorio::eleitos_por_genero() {
         if (c.get_cod_genero() == Genero::MASCULINO && c.is_eleito()) cont_masculino++;
         else if (c.get_cod_genero() == Genero::FEMININO && c.is_eleito()) cont_feminino++;
     }
-    /*
-                System.out.println("Feminino: " + eleitosFemininos + " (" + (eleitosFemininos + eleitosMasculinos == 0 ? "0" : 
-                                                    df.format((double) eleitosFemininos / (eleitosFemininos + eleitosMasculinos) * 100)) + "%)");
-        System.out.println("Masculino: " + eleitosMasculinos + " (" + (eleitosFemininos + eleitosMasculinos == 0 ? "0" : 
-                                                    df.format((double) eleitosMasculinos / (eleitosFemininos + eleitosMasculinos) * 100)) + "%)\n");
     
-    */
+    //         System.out.println("Feminino: " + cont_feminino + " (" + (cont_feminino + cont_masculino == 0 ? "0" : 
+    //                                             df.format((double) cont_feminino / (cont_feminino + cont_masculino) * 100)) + "%)");
+    // System.out.println("Masculino: " + cont_masculino + " (" + (cont_feminino + cont_masculino == 0 ? "0" : 
+    //                                             df.format((double)cont_masculino / (cont_feminino + cont_masculino) * 100)) + "%)\n");
+
+
+    cout << fixed << setprecision(2);
+
+    cout << "Feminino: " << cont_feminino << " (" << (cont_feminino + cont_masculino == 0 ? 0 : (double) cont_feminino / (cont_feminino + cont_masculino) * 100) << "%)\n";
+    cout << "Masculino: " << cont_masculino << " (" << (cont_feminino + cont_masculino == 0 ? 0 : (double) cont_masculino / (cont_feminino + cont_masculino) * 100) << "%)\n";
+    
+    
 }
 
 //Relatorio 10
@@ -92,7 +124,14 @@ void Relatorio::total_votos_validos() {
         votos_nominais += p.get_votos_nominais();
     }
     votos_validos = votos_legenda + votos_nominais;
-    // System.out.println("Total de votos válidos: " + nf.format(votosValidos));
-    // System.out.println("Total de votos nominais: " + nf.format(votosNominais) + " (" + (votosValidos == 0 ? "0" : df.format((double) votosNominais / votosValidos * 100)) + "%)");
-    // System.out.println("Total de votos de legenda: " + nf.format(votosLegenda) + " (" + (votosValidos == 0 ? "0" : df.format((double) votosLegenda / votosValidos * 100)) + "%)");
+    // System.out.println("Total de votos válidos: " + nf.format(votos_validos));
+    // System.out.println("Total de votos nominais: " + nf.format(votos_nominais) + " (" + (votos_validos == 0 ? "0" : df.format((double) votos_nominais / votos_validos * 100)) + "%)");
+    // System.out.println("Total de votos de legenda: " + nf.format(votos_legenda) + " (" + (votos_validos == 0 ? "0" : df.format((double) votos_legenda / votos_validos * 100)) + "%)");
+
+    cout << fixed << setprecision(2);
+
+    cout << "Total de votos válidos: " << votos_validos << "\n";
+    cout << "Total de votos nominais: " << votos_nominais << " (" << (votos_validos == 0 ? 0 : (double) votos_nominais / votos_validos * 100) << "%)\n";
+    cout << "Total de votos de legenda: " << votos_legenda << " (" << (votos_validos == 0 ? 0 : (double) votos_legenda / votos_validos * 100) << "%)\n";
+
 }
